7673ac0e77eb0fb68298dded4dedc425e5ed7fad
João <jpfsalviano@gmail.com>
2019-06-19 17:54:15 -0300

    1047 logic

--- 1047.py
+++ 1047.py
@@ -25,3 +25,43 @@ if initialHour < finalHour: # 01:00 / 02:00
         minute = 60 - (initialMinute - finalMinute)
    
 print('O JOGO DUROU ' + str(hour) + ' HORA(S) E ' + str(minute) + ' MINUTO(S)')
+
+
+'''
+
+hi == hf:
+    mi == mf:       # 24 horas
+        h= 24
+        m = 00
+    mi > mf:        # 23 horas
+        h = 23
+        m = 60-(mi-mf)
+    mi < mf:        # 0 hora
+        h = 0
+        m = mf - mi
+
+hi < hf:            # jogatina dentro de um mesmo dia
+    h = hf - hi
+    mi == mf:
+        h = h
+        m = mi - mf
+    mi > mf:
+        h = h
+        m = mi - mf
+    mi < mf:
+        h = h - 1
+        m = 60-(mf-mi)
+
+hi > hf:            # jogatina de um dia para o outro
+    h = 24 - hi + hf
+    mi == mf:
+        h = h
+        m = mi - mf
+    mi > mf:
+        h = h
+        m = mi - mf
+    mi < mf:
+        h = h - 1
+        m = 60-(mf-mi)    
+
+'''
